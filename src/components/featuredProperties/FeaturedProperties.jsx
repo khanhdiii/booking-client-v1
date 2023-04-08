96893f9d32a93d5080a9dd3fac9528780c751038
khanhdiii <khanh12369874@gmail.com>
2023-04-09 03:40:51 +0700

    fix err func when map

--- src/components/featuredProperties/FeaturedProperties.jsx
+++ src/components/featuredProperties/FeaturedProperties.jsx
@@ -12,22 +12,24 @@ const FeaturedProperties = () => {
         "loading..."
       ) : (
         <>
-          {data.map((item) => (
-            <div className="fpItem" key={item._id}>
-              <img src={item.photo[0]} alt="" className="fpImg" />
-              <span className="fpName">{item.name}</span>
-              <span className="fpCity">{item.city}</span>
-              <span className="fpPrice">
-                Starting from ${item.cheapestPrice}
-              </span>
-              {item.rating && (
-                <div className="fpRating">
-                  <button>{item.rating}</button>
-                  <span>Excellent</span>
+          {!data
+            ? "Loading..."
+            : data.map((item) => (
+                <div className="fpItem" key={item._id}>
+                  <img src={item.photo[0]} alt="" className="fpImg" />
+                  <span className="fpName">{item.name}</span>
+                  <span className="fpCity">{item.city}</span>
+                  <span className="fpPrice">
+                    Starting from ${item.cheapestPrice}
+                  </span>
+                  {item.rating && (
+                    <div className="fpRating">
+                      <button>{item.rating}</button>
+                      <span>Excellent</span>
+                    </div>
+                  )}
                 </div>
-              )}
-            </div>
-          ))}
+              ))}
         </>
       )}
     </div>
