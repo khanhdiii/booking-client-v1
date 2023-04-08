173caf6ab4ee4ba452b2cd589a17e99337e37e07
khanhdiii <khanh12369874@gmail.com>
2023-04-09 03:56:16 +0700

    fix map in data.map

--- src/components/featuredProperties/FeaturedProperties.jsx
+++ src/components/featuredProperties/FeaturedProperties.jsx
@@ -12,25 +12,23 @@ const FeaturedProperties = () => {
         "loading..."
       ) : (
         <>
-          {!data
-            ? "Loading..."
-            : data &&
-              data.map((item) => (
-                <div className="fpItem" key={item._id}>
-                  <img src={item.photo[0]} alt="" className="fpImg" />
-                  <span className="fpName">{item.name}</span>
-                  <span className="fpCity">{item.city}</span>
-                  <span className="fpPrice">
-                    Starting from ${item.cheapestPrice}
-                  </span>
-                  {item.rating && (
-                    <div className="fpRating">
-                      <button>{item.rating}</button>
-                      <span>Excellent</span>
-                    </div>
-                  )}
-                </div>
-              ))}
+          {data &&
+            data.map((item) => (
+              <div className="fpItem" key={item._id}>
+                <img src={item.photo[0]} alt="" className="fpImg" />
+                <span className="fpName">{item.name}</span>
+                <span className="fpCity">{item.city}</span>
+                <span className="fpPrice">
+                  Starting from ${item.cheapestPrice}
+                </span>
+                {item.rating && (
+                  <div className="fpRating">
+                    <button>{item.rating}</button>
+                    <span>Excellent</span>
+                  </div>
+                )}
+              </div>
+            ))}
         </>
       )}
     </div>
