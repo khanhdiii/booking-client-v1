cc4ca1d36a6a9f584d62eda86d2808662b19d0d7
khanhdiii <khanh12369874@gmail.com>
2023-05-27 19:28:49 +0700

    fix ui

--- src/components/navbar/NavbarMenu.jsx
+++ src/components/navbar/NavbarMenu.jsx
@@ -6,10 +6,10 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Link, useNavigate } from "react-router-dom";
-import "./navbarmenu.css";
 import { useSelector } from "react-redux";
 import { useRef, useState } from "react";
 import { Container, Nav, NavDropdown, Navbar } from "react-bootstrap";
+import "./navbarmenu.css";
 
 const NavbarMenu = () => {
   const user = useSelector((state) => state.auth.login.currentUser);
