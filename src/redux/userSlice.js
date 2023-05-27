918797345841ab969c9b193beb45c66c6a7b2e9c
khanhdiii <khanh12369874@gmail.com>
2023-05-28 03:09:25 +0700

    fix userSlice

--- src/redux/userSlice.js
+++ src/redux/userSlice.js
@@ -1,29 +1,30 @@
-import { createSlice } from "@reduxjs/toolkit"
+import { createSlice } from "@reduxjs/toolkit";
 
 const userSlice = createSlice({
-    name: "user",
-    initialState: {
-        users: {
-            allUsers: null,
-            isFetching: false,
-            error: false
-        }
+  name: "user",
+  initialState: {
+    users: {
+      allUsers: null,
+      isFetching: false,
+      error: false,
     },
-    reducers: {
-        getUserStart: (state) => {
-            state.users.isFetching = true
-        },
-        getUserSuccess: (state, action) => {
-            state.users.isFetching = false
-            state.users.allUsers = action.payload
-        },
-        getUserFaliled: (state) => {
-            state.users.isFetching = false
-            state.users.error = true
-        }
-    }
-})
+  },
+  reducers: {
+    getUserStart: (state) => {
+      state.users.isFetching = true;
+    },
+    getUserSuccess: (state, action) => {
+      state.users.isFetching = false;
+      state.users.allUsers = action.payload;
+    },
+    getUserFailed: (state) => {
+      state.users.isFetching = false;
+      state.users.error = true;
+    },
+  },
+});
 
-export const { getUserStart, getUserSuccess, getUserFaliled } = userSlice.actions
+export const { getUserStart, getUserSuccess, getUserFaliled } =
+  userSlice.actions;
 
-export default userSlice.reducer
+export default userSlice.reducer;
