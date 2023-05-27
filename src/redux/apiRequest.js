4cb75eb5d4c93d24d22d35954e062f62c34fba2b
khanhdiii <khanh12369874@gmail.com>
2023-05-27 19:34:33 +0700

    fix signin

--- src/redux/apiRequest.js
+++ src/redux/apiRequest.js
@@ -1,35 +1,44 @@
-import axios from "axios"
-import { loginFailed, loginStart, loginSuccess, registerFailed, registerStart, registerSuccess } from "./authSlice"
-import { getUserFaliled, getUserStart, getUserSuccess } from "./userSlice"
+import axios from "axios";
+import {
+  loginFailed,
+  loginStart,
+  loginSuccess,
+  registerFailed,
+  registerStart,
+  registerSuccess,
+} from "./authSlice";
+import { getUserFaliled, getUserStart, getUserSuccess } from "./userSlice";
 
 export const loginUser = async (user, dispatch, navigate) => {
-    dispatch(loginStart())
-    try {
-        const res = await axios.post("auth/signin", user)
-        dispatch(loginSuccess(res.data))
-        navigate("/")
-    } catch (err) {
-        dispatch(loginFailed)
-    }
-}
+  dispatch(loginStart());
+  try {
+    const res = await axios.post("auth/signin", user);
+    dispatch(loginSuccess(res.data));
+    navigate("/");
+  } catch (err) {
+    dispatch(loginFailed);
+  }
+};
 
 export const registerUser = async (user, dispatch, navigate) => {
-    dispatch(registerStart())
-    try {
-        await axios.post("/auth/signup", user)
-        dispatch(registerSuccess())
-        navigate("/")
-    } catch (err) {
-        dispatch(registerFailed())
-    }
-}
+  dispatch(registerStart());
+  try {
+    await axios.post("auth/signup", user);
+    dispatch(registerSuccess());
+    navigate("/");
+  } catch (err) {
+    dispatch(registerFailed());
+  }
+};
 
 export const getAllUsers = async (accessToken, dispatch) => {
-    dispatch(getUserStart())
-    try {
-        const res = await axios.get("/users", { headers: { token: `Bearer ${accessToken}` } })
-        dispatch(getUserSuccess(res.data))
-    } catch (err) {
-        dispatch(getUserFaliled())
-    }
-}
+  dispatch(getUserStart());
+  try {
+    const res = await axios.get("/users", {
+      headers: { token: `Bearer ${accessToken}` },
+    });
+    dispatch(getUserSuccess(res.data));
+  } catch (err) {
+    dispatch(getUserFaliled());
+  }
+};
